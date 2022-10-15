a042ea5b538a0cf5f73982e73104fc89a7194d85
Akeem <akeem.mighty@gmail.com>
2022-10-14 23:06:28 -0400

    Imported LinkedList Class + Created Books Object Of Class LinkedList

--- JavaLinkedList.java
+++ JavaLinkedList.java
@@ -1,5 +1,7 @@
+import java.util.LinkedList;
+
 public class JavaLinkedList {
     public static void main (String[] args) {
-        
+        LinkedList<String> books = new LinkedList<String>();
     }
 }
