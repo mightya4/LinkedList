efb6be5b6bffb8645c42e63d0eff6f3f2bfc5c26
Akeem <akeem.mighty@gmail.com>
2022-10-14 23:11:18 -0400

    Added Books To Books LinkedList Collection + Output Each Element in LinkedList

--- JavaLinkedList.java
+++ JavaLinkedList.java
@@ -3,5 +3,15 @@ import java.util.LinkedList;
 public class JavaLinkedList {
     public static void main (String[] args) {
         LinkedList<String> books = new LinkedList<String>();
+
+        books.add("To Kill a Mockingbird");
+        books.add("The Da Vinci Code");
+        books.add("The Hunger Games");
+        books.add("Water for Elephants");
+        books.add("Lord of the Flies");
+
+        for (String i: books) {
+            System.out.println(i);
+        }
     }
 }
